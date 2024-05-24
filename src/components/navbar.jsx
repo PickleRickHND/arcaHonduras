204789fa569f7ce0d820921e373e40a351c791d8
PickleRickHND <douglashedman@yahoo.es>
2024-05-23 21:48:27 -0600

    Improvements on the Navbar a& Footer (margins + dropdown on Desktop)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -26,21 +26,69 @@ class Navbar extends Component {
               <p>MENU</p>
               <li>
                 <a href="#1">¿Quienes Somos?</a>
+                <ul className="dropdown" id="dropdown">
+                  <li>
+                    <a href="#1-1">Historia6666666666666666666</a>
+                  </li>
+                  <li>
+                    <a href="#1-2">Equipo</a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#2">Misión y Visión</a>
+                <ul className="dropdown" id="dropdown">
+                  <li>
+                    <a href="#2-1">Nuestra Misión</a>
+                  </li>
+                  <li>
+                    <a href="#2-2">Nuestra Visión</a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#3">Nuestros Programas</a>
+                <ul className="dropdown" id="dropdown">
+                  <li>
+                    <a href="#3-1">Programa 1</a>
+                  </li>
+                  <li>
+                    <a href="#3-2">Programa 2</a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#4">¡Súmate!</a>
+                <ul className="dropdown" id="dropdown">
+                  <li>
+                    <a href="#4-1">Voluntariado</a>
+                  </li>
+                  <li>
+                    <a href="#4-2">Donaciones</a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#5">Colabora con Nosotros</a>
+                <ul className="dropdown" id="dropdown">
+                  <li>
+                    <a href="#5-1">Oportunidades</a>
+                  </li>
+                  <li>
+                    <a href="#5-2">Proyectos</a>
+                  </li>
+                </ul>
               </li>
               <li>
                 <a href="#6">Socios Estratégicos</a>
+                <ul className="dropdown" id="dropdown">
+                  <li>
+                    <a href="#6-1">Socio 1</a>
+                  </li>
+                  <li>
+                    <a href="#6-2">Socio 2</a>
+                  </li>
+                </ul>
               </li>
             </ul>
           </div>
