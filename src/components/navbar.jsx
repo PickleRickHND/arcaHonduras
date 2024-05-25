f2af0882ddec63309c0703b4437379571ffe362b
PickleRickHND <douglashedman@yahoo.es>
2024-05-24 22:20:20 -0600

    Navbar & Footer Components completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,29 @@ import "../components/css/navbar.css";
 import React, { Component } from "react";
 
 class Navbar extends Component {
-  state = { clicked: false };
+  state = {
+    mobileButton: false,
+    arrows: {
+      arrow1: false,
+      arrow2: false,
+      arrow3: false,
+      arrow4: false,
+      arrow5: false,
+      arrow6: false,
+    },
+  };
 
   handleHamburgerClick = () => {
-    this.setState({ clicked: !this.state.clicked });
+    this.setState({ mobileButton: !this.state.mobileButton });
+  };
+
+  handleArrowClick = (arrowName) => {
+    this.setState((prevState) => ({
+      arrows: {
+        ...prevState.arrows,
+        [arrowName]: !prevState.arrows[arrowName],
+      },
+    }));
   };
 
   render() {
@@ -21,14 +40,38 @@ class Navbar extends Component {
           <div>
             <ul
               id="navbar"
-              className={this.state.clicked ? "activeNavbar" : "#navbar"}
+              className={this.state.mobileButton ? "activeNavbar" : "#navbar"}
             >
               <p>MENU</p>
               <li>
                 <a href="#1">¿Quienes Somos?</a>
-                <ul className="dropdown" id="dropdown">
+                <div
+                  id="arrowHead_div"
+                  onClick={() => this.handleArrowClick("arrow1")}
+                >
+                  <i
+                    class={
+                      this.state.arrows["arrow1"]
+                        ? "fas fa-angle-down"
+                        : "fas fa-angle-right"
+                    }
+                  ></i>
+                </div>
+                <ul
+                  className={
+                    this.state.arrows.arrow1 ? "activeDropdown" : "dropdown"
+                  }
+                  id="dropdown"
+                >
+                  <p>¿QUIENES SOMOS?</p>
+                  <div
+                    id="backArrowHead_div"
+                    onClick={() => this.handleArrowClick("arrow1")}
+                  >
+                    <i class="fas fa-angle-left"></i>
+                  </div>
                   <li>
-                    <a href="#1-1">Historia6666666666666666666</a>
+                    <a href="#1-1">Historia6666666666</a>
                   </li>
                   <li>
                     <a href="#1-2">Equipo</a>
@@ -37,7 +80,31 @@ class Navbar extends Component {
               </li>
               <li>
                 <a href="#2">Misión y Visión</a>
-                <ul className="dropdown" id="dropdown">
+                <div
+                  id="arrowHead_div"
+                  onClick={() => this.handleArrowClick("arrow2")}
+                >
+                  <i
+                    class={
+                      this.state.arrows["arrow2"]
+                        ? "fas fa-angle-down"
+                        : "fas fa-angle-right"
+                    }
+                  ></i>
+                </div>
+                <ul
+                  className={
+                    this.state.arrows.arrow2 ? "activeDropdown" : "dropdown"
+                  }
+                  id="dropdown"
+                >
+                  <p>MISIÓN Y VISIÓN</p>
+                  <div
+                    id="backArrowHead_div"
+                    onClick={() => this.handleArrowClick("arrow2")}
+                  >
+                    <i class="fas fa-angle-left"></i>
+                  </div>
                   <li>
                     <a href="#2-1">Nuestra Misión</a>
                   </li>
@@ -48,7 +115,31 @@ class Navbar extends Component {
               </li>
               <li>
                 <a href="#3">Nuestros Programas</a>
-                <ul className="dropdown" id="dropdown">
+                <div
+                  id="arrowHead_div"
+                  onClick={() => this.handleArrowClick("arrow3")}
+                >
+                  <i
+                    class={
+                      this.state.arrows["arrow3"]
+                        ? "fas fa-angle-down"
+                        : "fas fa-angle-right"
+                    }
+                  ></i>
+                </div>
+                <ul
+                  className={
+                    this.state.arrows.arrow3 ? "activeDropdown" : "dropdown"
+                  }
+                  id="dropdown"
+                >
+                  <p>NUESTROS PROGRAMAS</p>
+                  <div
+                    id="backArrowHead_div"
+                    onClick={() => this.handleArrowClick("arrow3")}
+                  >
+                    <i class="fas fa-angle-left"></i>
+                  </div>
                   <li>
                     <a href="#3-1">Programa 1</a>
                   </li>
@@ -59,7 +150,31 @@ class Navbar extends Component {
               </li>
               <li>
                 <a href="#4">¡Súmate!</a>
-                <ul className="dropdown" id="dropdown">
+                <div
+                  id="arrowHead_div"
+                  onClick={() => this.handleArrowClick("arrow4")}
+                >
+                  <i
+                    class={
+                      this.state.arrows["arrow4"]
+                        ? "fas fa-angle-down"
+                        : "fas fa-angle-right"
+                    }
+                  ></i>
+                </div>
+                <ul
+                  className={
+                    this.state.arrows.arrow4 ? "activeDropdown" : "dropdown"
+                  }
+                  id="dropdown"
+                >
+                  <p>¡SÚMATE!</p>
+                  <div
+                    id="backArrowHead_div"
+                    onClick={() => this.handleArrowClick("arrow4")}
+                  >
+                    <i class="fas fa-angle-left"></i>
+                  </div>
                   <li>
                     <a href="#4-1">Voluntariado</a>
                   </li>
@@ -70,7 +185,31 @@ class Navbar extends Component {
               </li>
               <li>
                 <a href="#5">Colabora con Nosotros</a>
-                <ul className="dropdown" id="dropdown">
+                <div
+                  id="arrowHead_div"
+                  onClick={() => this.handleArrowClick("arrow5")}
+                >
+                  <i
+                    class={
+                      this.state.arrows["arrow5"]
+                        ? "fas fa-angle-down"
+                        : "fas fa-angle-right"
+                    }
+                  ></i>
+                </div>
+                <ul
+                  className={
+                    this.state.arrows.arrow5 ? "activeDropdown" : "dropdown"
+                  }
+                  id="dropdown"
+                >
+                  <p>COLABORA CON NOSOTROS</p>
+                  <div
+                    id="backArrowHead_div"
+                    onClick={() => this.handleArrowClick("arrow5")}
+                  >
+                    <i class="fas fa-angle-left"></i>
+                  </div>
                   <li>
                     <a href="#5-1">Oportunidades</a>
                   </li>
@@ -81,7 +220,31 @@ class Navbar extends Component {
               </li>
               <li>
                 <a href="#6">Socios Estratégicos</a>
-                <ul className="dropdown" id="dropdown">
+                <div
+                  id="arrowHead_div"
+                  onClick={() => this.handleArrowClick("arrow6")}
+                >
+                  <i
+                    class={
+                      this.state.arrows["arrow6"]
+                        ? "fas fa-angle-down"
+                        : "fas fa-angle-right"
+                    }
+                  ></i>
+                </div>
+                <ul
+                  className={
+                    this.state.arrows.arrow6 ? "activeDropdown" : "dropdown"
+                  }
+                  id="dropdown"
+                >
+                  <p>SOCIOS ESTRATÉGICOS</p>
+                  <div
+                    id="backArrowHead_div"
+                    onClick={() => this.handleArrowClick("arrow6")}
+                  >
+                    <i class="fas fa-angle-left"></i>
+                  </div>
                   <li>
                     <a href="#6-1">Socio 1</a>
                   </li>
@@ -96,7 +259,9 @@ class Navbar extends Component {
           <div id="mobileMenuButton_div" onClick={this.handleHamburgerClick}>
             <i
               id="hamburgerMenu"
-              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+              className={
+                this.state.mobileButton ? "fas fa-times" : "fas fa-bars"
+              }
             ></i>
           </div>
         </nav>
